abe892539e98542c8f49571dedb45a91aa640ff4
Balint Tamas <lordtomi18@gmail.com>
2025-04-21 17:52:21 +0200

    Updates

--- src/app/pages/animal-list/animal-list.component.ts
+++ src/app/pages/animal-list/animal-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimalService } from '../../services/animal.service';
-import { Animal } from '../../models/animal';
+
 @Component({
   selector: 'app-animal-list',
   imports: [],
@@ -8,14 +8,13 @@ import { Animal } from '../../models/animal';
   styleUrl: './animal-list.component.scss'
 })
 export class AnimalListComponent implements OnInit {
-
-  animals: Animal[] = [];
-
-  constructor(private animalService: AnimalService) {}
-
   ngOnInit(): void {
-    this.animalService.getAnimals().subscribe((data) => {
-      this.animals = data;
-    });
+    throw new Error('Method not implemented.');
   }
+
+ 
+
+  
+
+
 }
--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Animal } from '../models/animal';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
-  getAnimals(): Observable<Animal[]> {
-    // Később itt hívjuk majd a backendet
-    return of([
-      { id: 1, name: 'Bodri', breed: 'keverék', age: 4 },
-      { id: 2, name: 'Cirmi', breed: 'házi cica', age: 2 },
-    ]);
-  }
+  
+  
 }
 
